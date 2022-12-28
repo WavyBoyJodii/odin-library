386a086ce45a9c4e00aa1b375478d4a656fa9d5d
WavyBoyJodii <najeecasey@gmail.com>
2022-12-27 21:14:04 -0500

    Set CSS for page, build functions for library i believe i am doing the wrong thing in the add book to library function. i have to create objects using the new keyword which i have not. i also believe i am using object.create wrong  i shall refine

--- app.js
+++ app.js
@@ -1,16 +1,71 @@
 const myLibrary = [];
 
+const bookcase = document.querySelector('.bookcase');
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
+  this.info = () => {
     const infoString = `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
     return infoString;
   };
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author, pages, read) {
+  const myBook = Object.create(Book);
+  myBook.title = title;
+  myBook.author = author;
+  myBook.pages = pages;
+  myBook.read = read;
+  myLibrary.push(myBook);
 }
+
+const myFavBook = {
+  title: "The Setup",
+  author: "Dan Bilzerian",
+  pages: 500,
+  read: true,
+}
+myLibrary.push(myFavBook);
+// addBookToLibrary("moses", "King David", 888, true);
+// addBookToLibrary("Starships and their inventors", "jodissius", 1829, true);
+
+function displayBook(obj) {
+  for (let i = 0; i < obj.length; i++) {
+    const book = document.createElement('div');
+    book.classList.add('book');
+    for (let key in obj[i]) {
+    const item = document.createElement('p');
+    item.textContent = obj[i][key];
+    book.appendChild(item);
+    }  
+    bookcase.appendChild(book);
+  }
+}
+
+
+function addBookContents(obj) {
+  for (let key in obj) {
+    const item = document.createElement('p');
+    item.textContent = key;
+    book.appendChild(item);
+    // console.log(key);
+    }  
+}
+
+function displayObjectContents(obj) {
+  for (let key in obj) {
+    console.log(obj[key]);
+  }
+}
+
+// for (let key in obj) {
+//     const item = document.createElement('p');
+//     item.textContent = obj[key];
+//     book.appendChild(item);
+//   }  
+displayBook(myLibrary);
+displayObjectContents(myLibrary);
+
