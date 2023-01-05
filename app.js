3c4156320a33e98cb3022e5d74bea25f28acd853
WavyBoyJodii <najeecasey@gmail.com>
2023-01-05 16:47:58 -0500

    add javascript for form on page created new book button that makes a form pop up to input info for new book must figure how to submit this information on the form and use it to add a new book to the library and display it

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const myLibrary = [];
 
 const bookcase = document.querySelector('.bookcase');
+const addBookButton = document.getElementById('newBook');
+
+addBookButton.addEventListener('click', () => createBookForm());
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,13 +22,7 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(myBook);
 }
 
-// const myFavBook = {
-//   title: "The Setup",
-//   author: "Dan Bilzerian",
-//   pages: 500,
-//   read: true,
-// }
-// myLibrary.push(myFavBook);
+
 addBookToLibrary("moses", "King David", 888, true);
 addBookToLibrary("Starships and their inventors", "jodissius", 1829, true);
 
@@ -34,35 +31,76 @@ function displayBook(obj) {
     const disp = document.createElement('div');
     disp.classList.add('book');
     for (let key in book) {
-      const item = document.createElement('p');
-      item.textContent = `${key}: ${book[key]}`;
-      disp.appendChild(item);
+      const page = document.createElement('div');
+      const item1 = document.createElement('p');
+      item1.style.gridTemplateAreas = 'title';
+      const item2 = document.createElement('p')
+      item2.style.gridTemplateAreas = 'data';
+      item1.textContent = `${key}: `;
+      item2.textContent = `${book[key]}`;
+      page.append(item1, item2)
+      page.classList.add('divider')
+      disp.appendChild(page);
       }
     bookcase.appendChild(disp);
     }
   }
 
-
-function addBookContents(obj) {
-  for (let key in obj) {
-    const item = document.createElement('p');
-    item.textContent = key;
-    book.appendChild(item);
-    // console.log(key);
-    }  
-}
-
-function displayObjectContents(obj) {
-  for (let key in obj) {
-    console.log(obj[key]);
+  function createBookForm() {
+    const form = document.createElement('form');
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "example.com/path");
+    
+    const titleLabel = document.createElement('label');
+    titleLabel.setAttribute('for', 'title');
+    titleLabel.textContent = 'Title:';
+    const title = document.createElement('input');
+    title.setAttribute('type', 'text');
+    title.setAttribute('id', 'title');
+    title.setAttribute('name', 'title');
+    
+    const authorLabel = document.createElement('label');
+    authorLabel.setAttribute('for', 'author');
+    authorLabel.textContent = 'Author:';
+    const author = document.createElement('input');
+    author.setAttribute('type', 'text');
+    author.setAttribute('id', 'author');
+    author.setAttribute('name', 'author');
+    
+    const pagesLabel = document.createElement('label');
+    pagesLabel.setAttribute('for', 'pages');
+    pagesLabel.textContent = 'Pages:';
+    const pages = document.createElement('input');
+    pages.setAttribute('type', 'number');
+    pages.setAttribute('id', 'pages');
+    pages.setAttribute('name', 'pages');
+    
+    const noReadLabel = document.createElement('label');
+    noReadLabel.setAttribute('for', 'noRead');
+    noReadLabel.textContent = "Didn't Read";
+    const noRead = document.createElement('input');
+    noRead.setAttribute('type', 'radio');
+    noRead.setAttribute('id', 'noRead');
+    noRead.setAttribute('name', 'read');
+    noRead.setAttribute('value', 'false');
+    
+    const readLabel = document.createElement('label');
+    readLabel.setAttribute('for', 'read');
+    readLabel.textContent = "Read";
+    const read = document.createElement('input');
+    read.setAttribute('type', 'radio');
+    read.setAttribute('id', 'read');
+    read.setAttribute('name', 'read');
+    read.setAttribute('value', 'true');
+    
+    const formButton = document.createElement('button');
+    formButton.textContent = 'Submit';
+    
+    form.append(titleLabel, title, authorLabel, author, pagesLabel, pages, noReadLabel, noRead,
+    readLabel, read, formButton);
+    bookcase.appendChild(form);
   }
-}
 
-// for (let key in obj) {
-//     const item = document.createElement('p');
-//     item.textContent = obj[key];
-//     book.appendChild(item);
-//   }  
 displayBook(myLibrary);
-// displayObjectContents(myLibrary);
+
 
