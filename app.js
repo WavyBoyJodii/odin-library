db9c8eccf9c931006e8530ea43ffdadaf2a22604
WavyBoyJodii <najeecasey@gmail.com>
2023-01-06 19:43:34 -0500

    add button to change read status fix array index fixed the problem of not being able to correctly get the objects index in the array. also used hasOwnProperty to stop the display from populating with functions added only to the prototype and not the constructor

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-// Book.prototype.info = function () {
-//   const infoString = `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
-//   return infoString;
-// };
+Book.prototype.changeRead = function() {
+  let knowledge = this.read === true ? false : true;
+  this.read = knowledge;
+ };
 
 function addBookToLibrary(title, author, pages, read) {
   const myBook = new Book(title, author, pages, read);
+  console.log(Object.getPrototypeOf(myBook));
   myLibrary.push(myBook);
 }
-
 
 addBookToLibrary("moses", "King David", 888, true);
 addBookToLibrary("Starships and their inventors", "jodissius", 1829, true);
@@ -32,18 +32,30 @@ function removeAllChildNodes(parent) {
   }
 }
 
-function removeBook(e) {
-  let index = myLibrary.findIndex(books => books.title === this.title);
+function removeBook(x) {
+  let index = myLibrary.findIndex(books => books.title === x.title);
+  console.log(x);
+  console.log(index);
   myLibrary.splice(index, 1);
   displayBook(myLibrary);
-  }
+}
+
+function readUpdate(x) {
+  let index = myLibrary.findIndex(books => books.title === x.title);
+  console.log(index);
+  myLibrary[index].changeRead();
+  displayBook(myLibrary);
+}  
 
 function displayBook(obj) {
   removeAllChildNodes(bookcase);
   for (const book of obj) {
     const disp = document.createElement('div');
+    const source = book;
     disp.classList.add('book');
     for (let key in book) {
+      let isOwn = book.hasOwnProperty(key);
+      if (isOwn) {
       const page = document.createElement('div');
       const item1 = document.createElement('p');
       item1.style.gridTemplateAreas = 'title';
@@ -55,76 +67,83 @@ function displayBook(obj) {
       page.classList.add('divider')
       disp.appendChild(page);
       }
+    }
     bookcase.appendChild(disp);
     const removeButton = document.createElement('button');
     removeButton.classList.add('removeButton');
     removeButton.textContent = 'Delete';
-    removeButton.addEventListener('click', removeBook, false);
+    removeButton.addEventListener('click', () => removeBook(source), false);
     disp.appendChild(removeButton);
-    }
-  }
 
-  function createBookForm() {
-    const form = document.createElement('form');
-    form.setAttribute("method", "post");
-    form.setAttribute("action", "example.com/path");
-    
-    const titleLabel = document.createElement('label');
-    titleLabel.setAttribute('for', 'title');
-    titleLabel.textContent = 'Title:';
-    const title = document.createElement('input');
-    title.setAttribute('type', 'text');
-    title.setAttribute('id', 'title');
-    title.setAttribute('name', 'title');
-    
-    const authorLabel = document.createElement('label');
-    authorLabel.setAttribute('for', 'author');
-    authorLabel.textContent = 'Author:';
-    const author = document.createElement('input');
-    author.setAttribute('type', 'text');
-    author.setAttribute('id', 'author');
-    author.setAttribute('name', 'author');
-    
-    const pagesLabel = document.createElement('label');
-    pagesLabel.setAttribute('for', 'pages');
-    pagesLabel.textContent = 'Pages:';
-    const pages = document.createElement('input');
-    pages.setAttribute('type', 'number');
-    pages.setAttribute('id', 'pages');
-    pages.setAttribute('name', 'pages');
-    
-    const noReadLabel = document.createElement('label');
-    noReadLabel.setAttribute('for', 'noRead');
-    noReadLabel.textContent = "Didn't Read";
-    const noRead = document.createElement('input');
-    noRead.setAttribute('type', 'radio');
-    noRead.setAttribute('id', 'noRead');
-    noRead.setAttribute('name', 'read');
-    noRead.setAttribute('value', 'false');
-    
-    const readLabel = document.createElement('label');
-    readLabel.setAttribute('for', 'read');
-    readLabel.textContent = "Read";
-    const read = document.createElement('input');
-    read.setAttribute('type', 'radio');
-    read.setAttribute('id', 'read');
-    read.setAttribute('name', 'read');
-    read.setAttribute('value', 'true');
-    
-    const formButton = document.createElement('button');
-    formButton.textContent = 'Submit';
-    formButton.addEventListener('click', submitToLibrary, false);
-  
-    function submitToLibrary(event) {
-      event.preventDefault();
-      addBookToLibrary(title.value, author.value, pages.value, read.value);
-      displayBook(myLibrary);
-      }
-    
-    form.append(titleLabel, title, authorLabel, author, pagesLabel, pages, noReadLabel, noRead,
-    readLabel, read, formButton);
-    bookcase.appendChild(form);
+    const changeButton = document.createElement('button');
+    changeButton.classList.add('changeButton');
+    changeButton.textContent = 'Read Status';
+    changeButton.addEventListener('click', () => readUpdate(source), false);
+    disp.appendChild(changeButton);
   }
+}
+
+function createBookForm() {
+  const form = document.createElement('form');
+  form.setAttribute("method", "post");
+  form.setAttribute("action", "example.com/path");
+  
+  const titleLabel = document.createElement('label');
+  titleLabel.setAttribute('for', 'title');
+  titleLabel.textContent = 'Title:';
+  const title = document.createElement('input');
+  title.setAttribute('type', 'text');
+  title.setAttribute('id', 'title');
+  title.setAttribute('name', 'title');
+    
+  const authorLabel = document.createElement('label');
+  authorLabel.setAttribute('for', 'author');
+  authorLabel.textContent = 'Author:';
+  const author = document.createElement('input');
+  author.setAttribute('type', 'text');
+  author.setAttribute('id', 'author');
+  author.setAttribute('name', 'author');
+    
+  const pagesLabel = document.createElement('label');
+  pagesLabel.setAttribute('for', 'pages');
+  pagesLabel.textContent = 'Pages:';
+  const pages = document.createElement('input');
+  pages.setAttribute('type', 'number');
+  pages.setAttribute('id', 'pages');
+  pages.setAttribute('name', 'pages');
+    
+  const noReadLabel = document.createElement('label');
+  noReadLabel.setAttribute('for', 'noRead');
+  noReadLabel.textContent = "Didn't Read";
+  const noRead = document.createElement('input');
+  noRead.setAttribute('type', 'radio');
+  noRead.setAttribute('id', 'noRead');
+  noRead.setAttribute('name', 'read');
+  noRead.setAttribute('value', 'false');
+    
+  const readLabel = document.createElement('label');
+  readLabel.setAttribute('for', 'read');
+  readLabel.textContent = "Read";
+  const read = document.createElement('input');
+  read.setAttribute('type', 'radio');
+  read.setAttribute('id', 'read');
+  read.setAttribute('name', 'read');
+  read.setAttribute('value', 'true');
+    
+  const formButton = document.createElement('button');
+  formButton.textContent = 'Submit';
+  formButton.addEventListener('click', submitToLibrary, false);
+  
+  function submitToLibrary(event) {
+    event.preventDefault();
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
+    displayBook(myLibrary);
+    }
+    
+  form.append(titleLabel, title, authorLabel, author, pagesLabel, pages, noReadLabel, noRead,
+  readLabel, read, formButton);
+  bookcase.appendChild(form);
+}
 
 displayBook(myLibrary);
 
